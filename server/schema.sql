598c748692632cb98b2d0d11321083da2808a9f0
Jorge Castello <jorgio.castello@gmail.com>
2020-04-15 20:45:37 -0700

    Implement SQL schema for messages

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,11 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageId INT auto_increment primary key,
+  messageText VARCHAR(255),
+  user VARCHAR(30),
+  room VARCHAR(30),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 /* Create other tables and define schemas for them here! */
